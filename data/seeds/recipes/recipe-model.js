8b7590b89e2223f1805063dda31b300240f6d9b7
AjileJ <53097022+AjileJ@users.noreply.github.com>
2019-11-07 18:59:47 -0800

    done

--- data/seeds/recipes/recipe-model.js
+++ data/seeds/recipes/recipe-model.js
@@ -1,8 +1,30 @@
-const express = require("express");
+const db = require("../../../db-config");
 
-const db = require("./recipe.helper");
+module.exports = {
+  getRecipes,
+  getRecipesById,
+  getShoppingList,
+  getInstructions
+};
 
-const router = express.Router();
+function getRecipes() {
+  return db('recipes');
+}
+
+function getShoppingList(id) {
+  console.log(id);
+  return db('recipes_ingredients as ri')
+  .join('ingredients as i', 'ri.ingredients_id', 'i.id')
+  .where("ri.recipe_id",'=', id )
+  .select('i.name', 'ri.quantity', 'ri.amount') 
+}
+
+function getInstructions(id) {
+  return db('recipe_steps')
+  .join('recipes', 'recipe_steps.recipes_id', 'recipes.id')
+  .where('recipe.id', '=', id)
+  .select('recipe_steps.steps', 'recipe_steps.instructions')
+}
 
 
-module.exports = router;
+
